876a69019bc178d81c3775853c458856d66e18da
Rajasekar V <47376271+Raj252@users.noreply.github.com>
2019-03-08 18:12:21 +0530

    Create GFG.java

---
+++ GFG.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*; 
+
+class gfg{ 
+public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);  
+        String number = s.next(); 
+        boolean numeric = true;
+
+        try 
+        {
+            Double num = Double.parseDouble(number);
+        } 
+        catch (NumberFormatException e) 
+        {
+            numeric = false;
+        }
+
+        if(numeric)
+          {
+          System.out.println("yes");
+          }
+        else
+        {
+            System.out.println("No");
+        }
+    }
+} 
+
